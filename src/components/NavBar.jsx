7c7bff47ba913633bad5a7b715a00a8f5c768a7e
Sai Mittapalli <saimittapalli11@gmail.com>
2025-02-08 17:55:14 -0500

    navBar update

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { HStack, Image } from '@chakra-ui/react'
-import { Tabs, TabList, TabPanels, Tab, TabPanel, TabIndicator } from '@chakra-ui/react'
-import logo from '../assets/logo.png'
-import { Link, useLocation } from 'react-router-dom'
-import ContractsView from '../routes/ContractsView.jsx'
-import ProfilesView from '../routes/ProfilesView.jsx'
-import { useState } from 'react'
-import AuthContext from '../context/AuthContext.jsx'
+import React, { useEffect, useState } from 'react';
+import { HStack, Image, Button } from '@chakra-ui/react';
+import { Tabs, TabList, TabPanels, Tab, TabPanel, TabIndicator } from '@chakra-ui/react';
+import { Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react';
+import { ChevronDownIcon } from '@chakra-ui/icons';
+import { Link, useLocation } from 'react-router-dom';
+import logo from '../assets/logo.png';
+import { Icon } from '@chakra-ui/react';
+import { MdSettings } from 'react-icons/md';
 
 const NavBar = () => {
     const location = useLocation();
@@ -20,12 +20,12 @@ const NavBar = () => {
     };
 
     // Update selected index based on current route
-    React.useEffect(() => {
-        setSelectedIndex(tabMapping[location.pathname] || 0);
+    useEffect(() => {
+        setSelectedIndex(tabMapping[location.pathname] ?? 0);
     }, [location.pathname]);
 
     return (
-        <HStack justify="space-between" p={4} borderBottom="1px solid gray">
+        <HStack justify="space-between" p={4} borderBottom="1px solid white">
             <Image src={logo} boxSize="50px" />
             <Tabs
                 position="relative"
@@ -34,22 +34,30 @@ const NavBar = () => {
                 onChange={(index) => setSelectedIndex(index)}
             >
                 <TabList>
-                    <Tab _hover={{ color: 'blue.500' }}>
-                        <Link to="/home">Home</Link>
+                    <Tab as={Link} to="/" color={'white'} _hover={{ color: 'blue.500' }}>
+                        Home
                     </Tab>
-                    <Tab _hover={{ color: 'blue.500' }}>
-                        <Link to="/contractsView">Contracts</Link>
-                    </Tab>
-                    <Tab _hover={{ color: 'blue.500' }}>
-                        <Link to="/profilesView">Profile</Link>
+                    <Tab as={Link} to="/contractsView" color={'white'} _hover={{ color: 'blue.500' }}>
+                        Contracts
                     </Tab>
                 </TabList>
 
                 {/* Tab Indicator for Active Tab */}
                 <TabIndicator mt="-1.5px" height="2px" bg="blue.500" borderRadius="1px" />
             </Tabs>
+
+            {/* Move Menu Outside Tabs */}
+            <Menu>
+                <MenuButton as={Button} colorScheme='white' rightIcon={<ChevronDownIcon />}>
+                    <Icon as={MdSettings} />
+                </MenuButton>
+                <MenuList>
+                    <MenuItem color={'black'} as={Link} to="/profilesView">Profile</MenuItem>
+                    <MenuItem color={'black'}>Wallet</MenuItem>
+                </MenuList>
+            </Menu>
         </HStack>
     );
 };
 
-export default NavBar
+export default NavBar;
